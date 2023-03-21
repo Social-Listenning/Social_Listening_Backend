05d10cd1af40b3ef488735ea890803e5a4d11e6b
Quản Minh Đức <58112725+KaiKhongNgu@users.noreply.github.com>
2023-03-22 00:52:01 +0700

    Get All Social Logs
    Get All Social Logs

--- src/modules/socialLogs/socialLog.module.ts
+++ src/modules/socialLogs/socialLog.module.ts
@@ -1,11 +1,13 @@
 import { PrismaModule } from 'src/config/database/database.config.module';
 import { SocketModule } from '../sockets/socket.module';
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SocialLogService } from './services/socialLog.service';
+import { SocialLogController } from './controllers/socialLog.controller';
+import { SocialGroupModule } from '../socialGroups/socialGroup.module';
 
 @Module({
-  imports: [SocketModule, PrismaModule],
-  controllers: [],
+  imports: [SocketModule, PrismaModule, forwardRef(() => SocialGroupModule)],
+  controllers: [SocialLogController],
   providers: [SocialLogService],
   exports: [SocialLogService],
 })
