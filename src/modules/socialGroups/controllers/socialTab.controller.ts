b804afcbc2cb17ea7312d7a9073609b3c830faa8
Quản Minh Đức <58112725+KaiKhongNgu@users.noreply.github.com>
2023-04-07 01:54:05 +0700

    Update API Get Working State and Fix API Update Working State

--- src/modules/socialGroups/controllers/socialTab.controller.ts
+++ src/modules/socialGroups/controllers/socialTab.controller.ts
@@ -1,14 +1,37 @@
-import { Controller, Delete, Param, Put, Req, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Put,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { SocialTabService } from '../services/socialTab.service';
 import { PermissionGuard } from 'src/modules/auth/guards/permission.guard';
 import { SocialTabPerm } from '../enum/permission.enum';
 import { ReturnResult } from 'src/common/models/dto/returnResult';
 import { RequestWithUser } from 'src/modules/auth/interface/requestWithUser.interface';
 import { SocialTab } from '../models/socialGroup.model';
+import { APIKeyGuard } from 'src/modules/auth/guards/apikey.guard';
+import { WorkingState } from 'src/common/enum/workingState.enum';
 
 @Controller('socialTab')
 export class SocialTabController {
   constructor(private readonly socialTabService: SocialTabService) {}
+
+  @Get('/:id/working')
+  @UseGuards(APIKeyGuard)
+  async getTabWorkingState(@Param('id') id: string) {
+    const result = new ReturnResult<boolean>();
+    try {
+      const socialTab = await this.socialTabService.getSocialTabById(id);
+      result.result = socialTab.isWorked === WorkingState.Working;
+    } catch (error) {
+      result.message = error.message;
+    }
+    return result;
+  }
 
   @Put('/:id/working')
   @UseGuards(PermissionGuard(SocialTabPerm.updateWorkingStateTab.permission))
@@ -19,8 +42,9 @@ export class SocialTabController {
       if (!socialTab) throw new Error(`Not found the connection`);
       if (socialTab.delete) throw new Error(`Not found the connection`);
 
-      const state = socialTab.isWorked;
+      const state = !socialTab.isWorked;
       const tab = await this.socialTabService.updateWorkingStateById(id, state);
+      tab.isWorked = tab.isWorked === WorkingState.Working;
       result.result = tab;
     } catch (error) {
       result.message = error.message;
