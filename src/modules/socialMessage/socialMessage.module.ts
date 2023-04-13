8f2b56b0ba92091faa05d6a9e3d7803d640b7225
Quản Minh Đức <58112725+KaiKhongNgu@users.noreply.github.com>
2023-04-13 23:30:24 +0700

    Send Notify to User when the Message Come
    Send Notify to User when the Message Come

--- src/modules/socialMessage/socialMessage.module.ts
+++ src/modules/socialMessage/socialMessage.module.ts
@@ -5,11 +5,12 @@ import { SocialMessageController } from './controllers/socialMessage.controller'
 import { SocialGroupModule } from '../socialGroups/socialGroup.module';
 import { SocialPostService } from './services/socialPost.service';
 import { UserModule } from '../users/user.module';
+import { SocialMessageGateway } from './gateways/socialMessage.gateway';
 
 @Module({
   imports: [PrismaModule, SocialGroupModule, UserModule],
   controllers: [SocialMessageController],
-  providers: [SocialMessageService, SocialPostService],
+  providers: [SocialMessageService, SocialPostService, SocialMessageGateway],
   exports: [SocialMessageService, SocialPostService],
 })
 export class SocialMessageModule {}
